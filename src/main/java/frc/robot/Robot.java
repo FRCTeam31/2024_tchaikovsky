173414e6ab57a8cd884b7ac4873da27b196840b3
PoE309 <poe.kevin@gmail.com>
2024-01-12 19:40:23 -0600

    Configring AdvantageScope/Kit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,13 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+//import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.littletonrobotics.junction.LoggedRobot;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
