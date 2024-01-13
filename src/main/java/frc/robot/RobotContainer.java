3c56752daa8fd7cf15b771e86ac773fe125ce4d5
Zach <zacharyipursell@gmail.com>
2024-01-13 12:36:08 -0600

    Added a controls map, currently filling out robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,35 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.config.DriveMap;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
 
+  public Drivetrain Drivetrain;
+  public PowerDistribution Pdp;
+  public CommandJoystick DriverController;
+
   public RobotContainer() {
+    Pdp = new PowerDistribution();
+    Pdp.resetTotalEnergy();
+    Pdp.clearStickyFaults();
+    SmartDashboard.putData(Pdp);
+
+    Drivetrain = new Drivetrain(DriveMap.DrivetrainConfig);
+    Drivetrain.register();
+    SmartDashboard.putData(Drivetrain);
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    DriverController = new CommandJoystick(0);
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
