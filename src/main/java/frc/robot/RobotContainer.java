2a6064cb12768011d34cab66fc99a7519d6a8752
Zach <zacharyipursell@gmail.com>
2024-01-30 17:49:29 -0600

    Reconfigured robot PID

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -65,31 +65,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    AutoBuilder.configureHolonomic(
-      Drivetrain::getPose, // Robot pose supplier
-      Drivetrain::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-      Drivetrain::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-      Drivetrain::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-        new PIDConstants(0, 0, 0), // Translation PID constants
-        new PIDConstants(0, 0, 0), // Rotation PID constants
-        m_config.Drivetrain.MaxSpeedMetersPerSecond, // Max module speed, in m/s
-        m_config.Drivetrain.WheelBaseCircumferenceMeters / Math.PI / 2, // Drive base radius in meters. Distance from robot center to furthest module.
-        new ReplanningConfig() // Default path replanning config. See the API for the options here
-      ),
-      () -> {
-        // Boolean supplier that controls when the path will be mirrored for the red alliance
-        // This will flip the path being followed to the red side of the field.
-        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-        var alliance = DriverStation.getAlliance();
-        if (alliance.isPresent()) {
-          return alliance.get() == DriverStation.Alliance.Red;
-        }
-        return false;
-      },
-      Drivetrain // Reference to this subsystem to set requirements
-    );
     // Load the path you want to follow using its name in the GUI
     PathPlannerPath path = PathPlannerPath.fromPathFile("line 1m");
     Drivetrain.resetGyro();
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -82,31 +82,31 @@ public class Drivetrain extends SubsystemBase {
     m_snapToRotationController.setSetpoint(0);
 
     m_inHighGear = m_config.Drivetrain.StartInHighGear;
-    // AutoBuilder.configureHolonomic(
-    //   this::getPose, // Robot pose supplier
-    //   this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-    //   this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-    //   this::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-    //   new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-    //     new PIDConstants(0, 0, 0), // Translation PID constants
-    //     new PIDConstants(0, 0, 0), // Rotation PID constants
-    //     m_config.Drivetrain.MaxSpeedMetersPerSecond, // Max module speed, in m/s
-    //     m_config.Drivetrain.WheelBaseCircumferenceMeters / Math.PI / 2, // Drive base radius in meters. Distance from robot center to furthest module.
-    //     new ReplanningConfig() // Default path replanning config. See the API for the options here
-    //   ),
-    //   () -> {
-    //     // Boolean supplier that controls when the path will be mirrored for the red alliance
-    //     // This will flip the path being followed to the red side of the field.
-    //     // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+    AutoBuilder.configureHolonomic(
+      this::getPose, // Robot pose supplier
+      this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+      this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      this::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        new PIDConstants(0, 0, 0), // Translation PID constants
+        new PIDConstants(0, 0, 0), // Rotation PID constants
+        m_config.Drivetrain.MaxSpeedMetersPerSecond, // Max module speed, in m/s
+        m_config.Drivetrain.WheelBaseCircumferenceMeters / Math.PI / 2, // Drive base radius in meters. Distance from robot center to furthest module.
+        new ReplanningConfig() // Default path replanning config. See the API for the options here
+      ),
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-    //     var alliance = DriverStation.getAlliance();
-    //     if (alliance.isPresent()) {
-    //       return alliance.get() == DriverStation.Alliance.Red;
-    //     }
-    //     return false;
-    //   },
-    //   this // Reference to this subsystem to set requirements
-    // );
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this // Reference to this subsystem to set requirements
+    );
   }
 
   /**
