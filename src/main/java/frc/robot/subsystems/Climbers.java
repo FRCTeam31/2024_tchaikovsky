c50dc6ac7681f9cf7f3289fae5679e43c327aba2
mason <35619771+zeroClearAmerican@users.noreply.github.com>
2024-02-13 18:36:01 -0600

    added limit switches to climbers

--- src/main/java/frc/robot/subsystems/Climbers.java
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.config.RobotConfig;
@@ -9,31 +14,58 @@ import java.util.function.DoubleSupplier;
 
 public class Climbers extends SubsystemBase {
 
+  private ShuffleboardTab d_tab = Shuffleboard.getTab("Climbers");
+  private GenericEntry d_leftClimberEntry = d_tab
+    .add("Left Climber Limit Switch", false)
+    .withWidget(BuiltInWidgets.kBooleanBox)
+    .getEntry();
+  private GenericEntry d_rightClimberEntry = d_tab
+    .add("Right Climber Limit Switch", false)
+    .withWidget(BuiltInWidgets.kBooleanBox)
+    .getEntry();
+  private GenericEntry d_climbControlsActiveEntry = d_tab
+    .add("Climb Controls Active", false)
+    .withWidget(BuiltInWidgets.kBooleanBox)
+    .getEntry();
+
   private VictorSPX m_leftVictorSPX;
   private VictorSPX m_rightVictorSPX;
-  private boolean toggleClimber = false;
+  private DigitalInput m_leftLimitSwitch;
+  private DigitalInput m_rightLimitSwitch;
+
+  private boolean m_toggleClimber = false;
 
   // Creates the Climbers
   public Climbers(RobotConfig robotConfig) {
     m_leftVictorSPX = new VictorSPX(robotConfig.m_climbersVictorSPXLeftCanID);
     m_rightVictorSPX = new VictorSPX(robotConfig.m_climbersVictorSPXRightCanID);
+    m_leftLimitSwitch = new DigitalInput(0);
+    m_leftLimitSwitch = new DigitalInput(1);
   }
 
   //#region Methods
   // Raises the Left Climber
   public void raiseLeftArm() {
-    m_leftVictorSPX.set(
-      VictorSPXControlMode.PercentOutput,
-      RobotConfig.m_climbSpeed
-    );
+    if (!m_leftLimitSwitch.get()) {
+      m_leftVictorSPX.set(
+        VictorSPXControlMode.PercentOutput,
+        RobotConfig.m_climbSpeed
+      );
+    } else {
+      stopLeftArm();
+    }
   }
 
   // Raises the Right Climber
   public void raiseRightArm() {
-    m_rightVictorSPX.set(
-      VictorSPXControlMode.PercentOutput,
-      RobotConfig.m_climbSpeed
-    );
+    if (!m_rightLimitSwitch.get()) {
+      m_rightVictorSPX.set(
+        VictorSPXControlMode.PercentOutput,
+        RobotConfig.m_climbSpeed
+      );
+    } else {
+      stopRightArm();
+    }
   }
 
   // Lowers the Left Climber
@@ -64,14 +96,21 @@ public class Climbers extends SubsystemBase {
 
   //#endregion
 
+  @Override
+  public void periodic() {
+    d_leftClimberEntry.setBoolean(m_leftLimitSwitch.get());
+    d_rightClimberEntry.setBoolean(m_rightLimitSwitch.get());
+    d_climbControlsActiveEntry.setBoolean(m_toggleClimber);
+  }
+
   //#region Commands
   // Command for toggling the climbers on and off
   public Command toggleClimbersCommand() {
     return this.runOnce(() -> {
-        if (!toggleClimber) {
-          toggleClimber = true;
+        if (!m_toggleClimber) {
+          m_toggleClimber = true;
         } else {
-          toggleClimber = false;
+          m_toggleClimber = false;
         }
       });
   }
@@ -79,7 +118,7 @@ public class Climbers extends SubsystemBase {
   // Command for raising the Left Arm
   public Command raiseLeftArmCommand() {
     return this.run(() -> {
-        if (toggleClimber) {
+        if (m_toggleClimber) {
           raiseLeftArm();
         }
       });
@@ -88,7 +127,7 @@ public class Climbers extends SubsystemBase {
   // Command  for raising the Right Arm
   public Command raiseRightArmCommand() {
     return this.run(() -> {
-        if (toggleClimber) {
+        if (m_toggleClimber) {
           raiseRightArm();
         }
       });
@@ -100,7 +139,7 @@ public class Climbers extends SubsystemBase {
     DoubleSupplier rightClimber
   ) {
     return this.run(() -> {
-        if (toggleClimber) {
+        if (m_toggleClimber) {
           if (leftClimber.getAsDouble() > 0.5) {
             lowerLeftArm();
           } else if (rightClimber.getAsDouble() > 0.5) {
