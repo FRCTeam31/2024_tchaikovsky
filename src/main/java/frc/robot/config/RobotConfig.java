11e38c548c78703803b5b5780050948aea09a4a0
Zach <zacharyipursell@gmail.com>
2024-02-17 15:09:18 -0600

    repeatbaly is within about 3 inches of desired location

--- src/main/java/frc/robot/config/RobotConfig.java
+++ src/main/java/frc/robot/config/RobotConfig.java
@@ -41,9 +41,9 @@ public class RobotConfig {
         new PrimePIDConstants(0.019, 0, 0, 0, 0.138, 0, 0.05), // Drive PID
         new PrimePIDConstants(2, 0, 0), // Steering PID
         new PrimePIDConstants(0, 0, 0), // SnapTo PID,
-        new PrimePIDConstants(3, 0, 0), // Pathing Translation PID
+        new PrimePIDConstants(4, 0, 0), // Pathing Translation PID
         // new PrimePIDConstants(0.0425, 0, 0.004)
-        new PrimePIDConstants(1.5, 0, 0), // Pathing Rotation PID
+        new PrimePIDConstants(2.5, 0, 0), // Pathing Rotation PID$
         0.15,
         0.5
       );
